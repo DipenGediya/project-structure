7a103023683cc3634b54546ebbed7ae8a5d6bc5a
DipenGediya <4360dipengediya@email.com>
2024-06-28 17:39:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Route, Routes } from "react-router-dom";
+import UserNavbar from "./users/header/Navbar";
+import Home from "./users/pages/Home";
+import About from "./users/pages/About";
+import Product from "./users/pages/Product";
+import AdminNavbar from "./admin/header/Navbar";
+import Dashborad from "./admin/pages/Dashborad";
+import Manage from "./admin/pages/Manage";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  let role = "admin";
+
+  if (role == "user") {
+    return (
+        <>
+          <UserNavbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/product" element={<Product />} />
+            <Route path="/:id" element={<AddToCart />} />
+          </Routes>
+        </>
+
+    );
+  } else if (role == "admin") {
+    return (
+        <>
+          <AdminNavbar />
+          <Routes>
+            <Route path="/" element={<Dashborad />} />
+            <Route path="/manage" element={<Manage />} />
+          </Routes>
+        </>
+    )
+  }
+
 }
 
 export default App;
